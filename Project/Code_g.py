412593a088da68b886c4fa8247cde327f3b2a3f7
Andrei Pricope <cristianandrei.pricope@man-es.com>
2023-11-15 09:49:20 +0100

    mare variabila

--- Project/Code_g.py
+++ Project/Code_g.py
@@ -19,23 +19,29 @@ random_node = "wikipediak4by6wf"
 
 taken_down = {}
 infected_neighbors = {}
+defense = {}
 for node in darkweb:
     if node == random_node:
         taken_down[node] = True  
         infected_neighbors = 0
+        defense = darkweb.degree[node]
     else:
         taken_down[node] = False
         infected_neighbors = 0
+        defense = darkweb.degree[node]
         
 nx.set_node_attributes(darkweb, taken_down, "taken_down")
 nx.set_node_attributes(darkweb, infected_neighbors, "infected_neighbors")
+nx.set_node_attributes(darkweb, defense, "defense")
 
 def break_nodes(graph):
     x = 0
     for node in graph:
         if graph.nodes[node]["taken_down"] == True and graph.nodes[node]["infected_neighbors"] != len(graph[node]):
-            graph.nodes[list(darkweb[node].keys())[graph.nodes[node]["infected_neighbors"]]]["taken_down"] = True
-            graph.nodes[node]["infected_neighbors"] += 1
+            graph.nodes[list(darkweb[node].keys())[graph.nodes[node]["infected_neighbors"]]]["defense"] -= 1
+            if graph.nodes[list(darkweb[node].keys())[graph.nodes[node]["infected_neighbors"]]]["defense"] <= 0:
+                graph.nodes[list(darkweb[node].keys())[graph.nodes[node]["infected_neighbors"]]]["taken_down"] = True
+                graph.nodes[node]["infected_neighbors"] += 1
             return
             
 for _ in range(0, 100):
