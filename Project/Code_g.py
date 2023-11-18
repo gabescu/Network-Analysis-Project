32b657cb552f5fdb0f19cf76a636726ed1096c03
Gabriel Catalin Ionescu <gabescu05@gmail.com>
2023-11-18 13:49:40 +0100

    timer

--- Project/Code_g.py
+++ Project/Code_g.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import operator
+import time
 
 
 #Reading
@@ -19,7 +20,7 @@ darkweb = nx.DiGraph()
 darkweb.add_weighted_edges_from(edges)
 nr_nodes = len(darkweb.nodes)
 
-#Function that returnes the node with maximum betweenness centrality
+#Function that returns the node with maximum betweenness centrality
 def max_betweenness_centrality(graph):
     betweenness = nx.betweenness_centrality(graph)
     max_b = max(betweenness.items(), key=operator.itemgetter(1))[0]
@@ -27,13 +28,13 @@ def max_betweenness_centrality(graph):
 
 #Function that returns the node with maximum pagerank value
 def max_pagerank(graph):
-    pagerank = nx.pagerank(graph, max_iter = 1000, weight = "weight")
+    pagerank = nx.pagerank(graph, max_iter = 7000, weight = "weight")
     max_p = max(pagerank.items(), key=operator.itemgetter(1))[0]
     return max_p
 
-#Function that removes the max degree node
+#Function that removes the interest node
 def attack_nodes():
-    max_centrality_node = max_betweenness_centrality(darkweb)
+    max_centrality_node = max_pagerank(darkweb)
     darkweb.remove_node(max_centrality_node)
 
 #Loop functions by steps times
@@ -41,11 +42,12 @@ print("Please select number of steps:")
 steps = int(input())
 darkweb_undirected = darkweb.to_undirected()
 i = 0
+start_time = time.time()
 while i < steps:
     attack_nodes()
     darkweb_undirected = darkweb.to_undirected()
     i += 1
 
 #Print number of connected components in the graph
-print(nx.number_connected_components(darkweb_undirected))
-    
+end_time = time.time()
+print(nx.number_connected_components(darkweb_undirected), f"Time of execution is {round(end_time - start_time, 3)} seconds")
